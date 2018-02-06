d2a4c7b80b487836db0fa01e54d2938e4cd7f623
Vytautas <viiskis@gmail.com>
2018-02-06 10:44:18 +0200

    bootstrap migration

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,18 +3,17 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-lg-6">
         <form>
             <div class="form-group">
                 <label for="title">Title</label>
                 <input type="text" class="form-control" id="title" placeholder="Title">
             </div>
-            
             <div class="form-group">
                 <label for="title">Content</label>
                 <textarea type="text" class="form-control" id="content" placeholder="Your blog content goes here..."></textarea>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-secondary">Submit</button>
         </form>
     </div>
 </div>
