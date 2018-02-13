ad1c7a12e05c4579a081972636abbf12a4994643
Vytautas <viiskis@gmail.com>
2018-02-13 09:35:19 +0200

    cleanup

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,28 +1,30 @@
 @extends('layouts.app') 
 
 @section('content')
-<div class="col-xl-6">
-    <form method="post" action="{{ route('posts.update', ['postid' => $post->id]) }}">
-        {!! csrf_field() !!}
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="Title" value="{{ $post->title }}">
-        </div>
-        @if ($errors->get('title')) 
-            @foreach ($errors->get('title') as $error)
-                <div class="alert alert-danger" role="alert">{{ $error }}</div>
-            @endforeach 
-        @endif
-        <div class="form-group">
-            <label for="title">Content</label>
-            <textarea type="text" name="content" class="form-control" id="content" placeholder="Blog content goes here...">{{$post->content}}</textarea>
-        </div>
-        @if ($errors->get('title')) 
-            @foreach ($errors->get('title') as $error)
-                <div class="alert alert-danger" role="alert">{{ $error }}</div>
-            @endforeach 
-        @endif
-        <button type="submit" class="btn btn-secondary">Submit</button>
-    </form>
+<div class="row justify-content-md-center mt-5">
+    <div class="col-xl-6">
+        <form method="post" action="{{ route( 'posts.update', [ 'postid' => $post->id ] ) }}">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" class="form-control" id="title" placeholder="Title" value="{{ $post->title }}">
+            </div>
+            @if ($errors->get( 'title' )) 
+                @foreach ($errors->get( 'title' ) as $error)
+                    <div class="alert alert-danger" role="alert">{{ $error }}</div>
+                @endforeach 
+            @endif
+            <div class="form-group">
+                <label for="title">Content</label>
+                <textarea type="text" name="content" class="form-control" id="content" placeholder="Blog content goes here...">{{ $post->content }}</textarea>
+            </div>
+            @if ( $errors->get( 'title' )) 
+                @foreach ($errors->get( 'title' ) as $error)
+                    <div class="alert alert-danger" role="alert">{{ $error }}</div>
+                @endforeach 
+            @endif
+            <button type="submit" class="btn btn-secondary">Submit</button>
+        </form>
+    </div>
 </div>
 @endsection
