c4b4525c7c281bbb1849a7d81a4cd006cb6b90ba
Vytautas <viiskis@gmail.com>
2018-02-13 13:15:40 +0200

    edits

--- resources/views/comments/form.blade.php
+++ resources/views/comments/form.blade.php
@@ -1,29 +1,31 @@
-<div class="col-xl-6">
-    <form method="post" action="{{ route('comments.store', [ 'post_id' => $post->id ]) }}">
-        {!! csrf_field() !!}
-
-        <div class="form-group">
-            <label for="cname">Your name</label>
-            <input type="text" name="cname" class="form-control" id="cname" placeholder="Your name" value="{{ old('cname') }}">
-        </div>
-
-        @if ($errors->get('cname'))
-            @foreach ($errors->get('cname') as $error)
-                <div class="alert alert-danger" role="alert">{{ $error }}</div>
-            @endforeach 
-        @endif
-
-        <div class="form-group">
-            <label for="content">Content</label>
-            <textarea type="text" name="content" class="form-control" id="content" placeholder="Your comment...">{{ old('content') }}</textarea>
-        </div>
-
-        @if ($errors->get('content')) 
-            @foreach ($errors->get('content') as $error)
-                <div class="alert alert-danger" role="alert">{{ $error }}</div>
-            @endforeach 
-        @endif
-        
-        <button type="submit" class="btn btn-secondary">Submit</button>
-    </form>
+<div class="row justify-content-md-center my-5">
+    <div class="col-xl-6">
+        <form method="post" action="{{ route('comments.store', [ 'post_id' => $post->id ]) }}">
+            {!! csrf_field() !!}
+            
+            <div class="form-group">
+                <label for="cname">Your name</label>
+                <input type="text" name="cname" class="form-control" id="cname" placeholder="Your name" value="{{ old('cname') }}">
+            </div>
+            
+            @if ($errors->get('cname'))
+                @foreach ($errors->get('cname') as $error)
+                    <div class="alert alert-danger" role="alert">{{ $error }}</div>
+                @endforeach 
+            @endif
+            
+            <div class="form-group">
+                <label for="content">Your comment</label>
+                <textarea type="text" name="content" class="form-control" id="content" placeholder="Your comment...">{{ old('content') }}</textarea>
+            </div>
+            
+            @if ($errors->get('content')) 
+                @foreach ($errors->get('content') as $error)
+                    <div class="alert alert-danger" role="alert">{{ $error }}</div>
+                @endforeach 
+            @endif
+            
+            <button type="submit" class="btn btn-secondary">Submit</button>
+        </form>
+    </div>
 </div>
--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
     @section('content')
+
     <div class="row justify-content-md-center mt-5">
         <div class="col-6">
             <h2>{{ $post->title }}</h2>
@@ -9,17 +10,20 @@
             <h4>{{ $post->date }}</h4>
         </div>
     </div>
+
     <div class="row justify-content-md-center">
         <div class="col-8">
             <p>{!! $post->content !!}</p>
         </div>
     </div>
+
     <div class="row justify-content-md-center mt-5">
         <div class="col-12 text-center">
-            <a href="{{ route('posts.edit', ['id' => $post->id]) }}"> Edit post </a> /
-            <a href="{{ route('posts.delete', ['id' => $post->id]) }}"> Delete a post </a>
+            <a href="{{ route('posts.edit', ['id' => $post->id]) }}">Edit post</a> /
+            <a href="{{ route('posts.delete', ['id' => $post->id]) }}">Delete a post</a>
         </div>
     </div>
+
     @include('partials.comments')
     @include('comments.form')
 
